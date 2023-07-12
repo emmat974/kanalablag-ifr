d279b6b5064a3eaa03fb34ecacbebaa33454da23
Emma Tony <emma.tony.georges@gmail.com>
2023-07-12 08:44:38 +0400

    mise a jour pagination + style bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Joke;
 use App\Repository\CategoryRepository;
 use App\Repository\JokeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,23 +18,56 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(
         CategoryRepository $categoryRepository,
-        JokeRepository $jokeRepository
+        EntityManagerInterface $em,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response {
+
+        $dql = "SELECT j FROM App\Entity\Joke j ORDER BY j.updatedAt DESC";
+        $query = $em->createQuery($dql);
+
+        $paginator = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
             'categories' => $categoryRepository->findall(),
-            'jokes' => $jokeRepository->findAll()
+            'jokes' => $paginator
         ]);
     }
 
-    #[Route('/categories/{id}', name: 'app_home_joke')]
+    #[Route('/categories/{id}', name: 'app_home_categories')]
     public function category(
-        Category $category
+        Category $category,
+        EntityManagerInterface $em,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response {
+
+        $dql = "SELECT j FROM App\Entity\Joke j JOIN j.category c WHERE c.id = :categoryId ORDER BY j.updatedAt DESC";
+        $query = $em->createQuery($dql);
+        $query->setParameter('categoryId', $category->getId());
+
+        $paginator = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
+
         return $this->render('home/categories.html.twig', [
-            'controller_name' => 'HomeController',
             'category' => $category,
-            'jokes' => $category->getJokes()
+            'jokes' => $paginator
+        ]);
+    }
+
+    #[Route("/blague/{id}", name: 'app_home_joke')]
+    public function blague(Joke $joke): Response
+    {
+        return $this->render('home/joke.html.twig', [
+            'joke' => $joke
         ]);
     }
 }
